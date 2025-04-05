bb00c83818346016b77c1fcfd44cdd1c571fdb68
Nadejda <nadyash295923@gmail.com>
2025-04-05 23:55:25 +0300

    hide toolbar for small devices

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -62,7 +62,7 @@ export const Toolbar = () => {
             <path d="M18 6L6 18M6 6L18 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
           </svg>
         ) : (
-          <div className="div"></div>
+          <div className="div">⚙️</div>
         )}
       </button>
       
